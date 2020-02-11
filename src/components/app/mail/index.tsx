fc2da4f910eee1738b3c2e62010658004e6082ac
Yohann Fin <yohann.fin13@gmail.com>
2020-02-11 09:56:31 +0100

    [FEAT] Add notification for new mail

--- src/components/app/mail/index.tsx
+++ src/components/app/mail/index.tsx
@@ -7,27 +7,46 @@ export interface IMailProps {
     sender?: string,
     topic?: string,
     date?: string,
+    notification: boolean,
     content?: string
 }
 
 const mailTestList: IMailProps[] = [
-    { sender: 'John Doe', topic: 'My First Mail', date: 'Hier', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vitae leo dapibus, accumsan lorem eleifend, pharetra quam. Quisque vestibulum commodo justo, eleifend mollis enim blandit eu. Aenean hendrerit nisl et elit maximus finibus. Suspendisse scelerisque consectetur nisl mollis scelerisque.' },
-    { sender: 'David Doe', topic: 'My Second Mail', date: 'Vendredi', content: 'Bonjour John, j\'ai récuperé le colis que tu m\'as envoyé. Merci beaucoup, cordialement David Doe' },
+    { sender: 'John Doe', topic: 'My First Mail', date: 'Hier', notification: true, content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vitae leo dapibus, accumsan lorem eleifend, pharetra quam. Quisque vestibulum commodo justo, eleifend mollis enim blandit eu. Aenean hendrerit nisl et elit maximus finibus. Suspendisse scelerisque consectetur nisl mollis scelerisque.' },
+    { sender: 'David Doe', topic: 'My Second Mail', date: 'Vendredi', notification: false, content: 'Bonjour John, j\'ai récuperé le colis que tu m\'as envoyé. Merci beaucoup, cordialement David Doe' },
  
 ]
 
 const Mail: React.FC = () => {
+    const getNotification = (etat: boolean) => {
+        if (etat)
+        {
+            return (
+                <div className="circle"></div>
+            )
+        }
+        else
+        {
+            return (
+                <div></div>
+            )
+        }
+    }
+
     const getMail = () => {
         return mailTestList.map((props) => {
             return (
-                <div className="item-mail">
-                    <hr/>
-                    <div className="item-mail-top">
-                        <div id="mail-sender">{props.sender}</div>
-                        <div id='mail-date'>{props.date}</div>
+                <div className="mail-core">
+                    {getNotification(props.notification)}
+                    <div className="item-mail">
+                        <hr/>
+                        <div className="item-mail-top">
+                            <div id="mail-sender">{props.sender}</div>
+                            <div id='mail-date'>{props.date}</div>
+                        </div>
+                        <div id="mail-topic">{props.topic}</div>
+                        <div id="mail-content">{props.content}</div>
                     </div>
-                    <div id="mail-topic">{props.topic}</div>
-                    <div id="mail-content">{props.content}</div>
                 </div>
             )
         })

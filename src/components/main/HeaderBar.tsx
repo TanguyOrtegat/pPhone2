633bc362bcaa38e460c451cc666b028612ea2cb3
Yohann Fin <yohann.fin13@gmail.com>
2020-02-11 14:39:33 +0100

    [FEAT] Add real time in header

--- src/components/main/HeaderBar.tsx
+++ src/components/main/HeaderBar.tsx
@@ -10,11 +10,36 @@ import location from '../../assets/icons/location.png'
 // TODO: We need to do something dynamic for the color of the header, as if the background is too light
 // it would be hard to read the text and the icons, we also should switch to black when using a "light theme" app
 
+type ClockState = {
+    time: Date
+}
+  
+export class TimeActually extends React.Component<{}, ClockState> {
+  
+    tick() {
+      this.setState({
+        time: new Date()
+      });
+    }
+  
+    componentWillMount() {
+      this.tick();
+    }
+  
+    componentDidMount() {
+      setInterval(() => this.tick(), 1000);
+    }
+  
+    render() {
+      return <span className="phone-header-time">{this.state.time.getHours()}:{this.state.time.getMinutes()}</span>
+    }
+  }
+
 export const HeaderBar: React.FC = () => {
     return (
         <div className="phone-header">
             <div className="phone-header-left">
-                <span className="phone-header-time">16:20</span>
+                <TimeActually/>
                 <div className="phone-header-icon" style={{ backgroundImage: `url(${location})`, float: 'right', width: '8px', marginTop: '2px' }}></div>
             </div>
 
